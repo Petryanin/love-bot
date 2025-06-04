d8b05e1ca1c98d7a6ea57274bca303c03fd90c16
as.petryanin <aspetryanin@gmail.com>
2025-06-05 00:20:52 +0200

    fix: scheduler ctx logic

--- internal/scheduler/scheduler.go
+++ internal/scheduler/scheduler.go
@@ -20,14 +20,21 @@ func StartPlanScheduler(
 		ticker := time.NewTicker(interval)
 		defer ticker.Stop()
 
-		for now := range ticker.C {
-			duePlans, err := app.Plan.GetDueAndMark(ctx, now)
-			if err != nil {
-				log.Printf("scheduler: failed to fetch plans: %v", err)
-				continue
-			}
-			for _, p := range duePlans {
-				handlers.PlansRemindHandler(&p, app)(ctx, b, nil)
+		for {
+			select {
+			case <-ctx.Done():
+				log.Println("PlanScheduler: context cancelled, stopping scheduler")
+				return
+
+			case now := <-ticker.C:
+				duePlans, err := app.Plan.GetDueAndMark(ctx, now)
+				if err != nil {
+					log.Printf("PlanScheduler: failed to fetch due plans: %v", err)
+					continue
+				}
+				for _, p := range duePlans {
+					handlers.PlansRemindHandler(&p, app)(ctx, b, nil)
+				}
 			}
 		}
 	}()
@@ -43,14 +50,21 @@ func StartCatScheduler(
 		ticker := time.NewTicker(interval)
 		defer ticker.Stop()
 
-		for now := range ticker.C {
-			dueUsers, err := app.User.FetchDueCats(context.Background(), now.UTC())
-			if err != nil {
-				log.Printf("scheduler: failed to fetch cats: %v", err)
-				continue
-			}
-			for _, u := range dueUsers {
-				handlers.ScheduledComplimentImageHandler(ctx, app, b, u.ChatID)
+		for {
+			select {
+			case <-ctx.Done():
+				log.Println("CatScheduler: context cancelled, stopping scheduler")
+				return
+
+			case now := <-ticker.C:
+				dueUsers, err := app.User.FetchDueCats(ctx, now.UTC())
+				if err != nil {
+					log.Printf("CatScheduler: failed to fetch due cats: %v", err)
+					continue
+				}
+				for _, u := range dueUsers {
+					handlers.ScheduledComplimentImageHandler(ctx, app, b, u.ChatID)
+				}
 			}
 		}
 	}()
